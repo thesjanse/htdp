ad9de5e3f6209838f1e575ec9da059134f728520
Alexander Mejinski <alexander.mejinski@gmail.com>
2020-08-19 21:08:20 +0300

    feature: add exercise 29

---
+++ chapter2/exercise29.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require rackunit)
+
+(define DEFAULT-PRICE 5.0)
+(define DEFAULT-ATTENDANCE 120)
+(define FIXED-COST 0)
+(define VARIABLE-COST 1.5)
+(define CHANGE-IN-PEOPLE 15)
+(define CHANGE-IN-PRICE 0.1)
+
+(define (avg-attendance ticket-price)
+    (- DEFAULT-ATTENDANCE (* (- ticket-price DEFAULT-PRICE)
+                             (/ CHANGE-IN-PEOPLE CHANGE-IN-PRICE))))
+
+(define (revenue ticket-price)
+    (* ticket-price (avg-attendance ticket-price)))
+
+(define (cost ticket-price)
+    (+ FIXED-COST (* VARIABLE-COST
+                     (avg-attendance ticket-price))))
+
+(define (profit ticket-price)
+    (- (revenue ticket-price)
+       (cost ticket-price)))
