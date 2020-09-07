27a0a3b457f6eedf0cb0a7174d06e3a6cc4f0b33
Alexander Mejinski <alexander.mejinski@gmail.com>
2020-09-07 21:28:27 +0300

    feature: add key events handler

--- chapter2/exercise47.rkt
+++ chapter2/exercise47.rkt
@@ -18,7 +18,7 @@
 ; HappinessLevel is a Number
 ; current level of pet happiness
 
-; Number -> HappinessLevel
+; HappinessLevel -> HappinessLevel
 ; deteriorate HappinessLevel with each clock tick
 ; can't go below 0
 
@@ -30,6 +30,24 @@
 (check-equal? (tick 100) (- 100 DETERIORATION-SPEED))
 (check-equal? (tick 50) (- 50 DETERIORATION-SPEED))
 (check-equal? (tick 0.1) 0.1)
+
+; HappinessLevel -> HappinessLevel
+; increase HappinessLevel by 1/3 of the current HappinessLevel
+; by pressing up arrow
+; increase HappinessLevel by 1/5 of the current HappinessLevel
+; by pressing down arrow
+
+(define (key-handler key hl)
+    (cond
+        [(key=? key "up") (+ hl (* hl 0.34))]
+        [(key=? key "down") (+ hl (* hl 0.2))]))
+
+(check-equal? (key-handler "up" 0) 0)
+(check-equal? (key-handler "up" 0.1) (+ 0.1 (* 0.1 0.34)))
+(check-equal? (key-handler "up" 10) (+ 10 (* 10 0.34)))
+(check-equal? (key-handler "down" 0) 0)
+(check-equal? (key-handler "down" 0.1) (+ 0.1 (* 0.1 0.2)))
+(check-equal? (key-handler "down" 10) (+ 10 (* 10 0.2)))
 
 ;;; (define (gauge max-happiness)
 ;;;     (big-bang max-happiness
